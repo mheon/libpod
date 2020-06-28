3c35fa297479308fa96d30836511a226185b5e72
Matthew Heon <matthew.heon@pm.me>
2020-06-28 10:54:42 -0400

    HACK HACK HACK Let's just use common HACK HACK HACK
    Signed-off-by: Matthew Heon <matthew.heon@pm.me>

--- pkg/api/handlers/compat/containers_create.go
+++ pkg/api/handlers/compat/containers_create.go
@@ -4,26 +4,23 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
-	"strings"
 
-	"github.com/containers/common/pkg/config"
+	"github.com/containers/libpod/cmd/podman/common"
 	"github.com/containers/libpod/libpod"
-	image2 "github.com/containers/libpod/libpod/image"
 	"github.com/containers/libpod/pkg/api/handlers"
 	"github.com/containers/libpod/pkg/api/handlers/utils"
-	"github.com/containers/libpod/pkg/namespaces"
-	"github.com/containers/libpod/pkg/signal"
-	createconfig "github.com/containers/libpod/pkg/spec"
-	"github.com/containers/storage"
+	"github.com/containers/libpod/pkg/domain/entities"
+	"github.com/containers/libpod/pkg/specgen"
+	"github.com/containers/libpod/pkg/specgen/generate"
 	"github.com/gorilla/schema"
 	"github.com/pkg/errors"
-	"golang.org/x/sys/unix"
 )
 
 func CreateContainer(w http.ResponseWriter, r *http.Request) {
 	runtime := r.Context().Value("runtime").(*libpod.Runtime)
 	decoder := r.Context().Value("decoder").(*schema.Decoder)
 	input := handlers.CreateContainerConfig{}
+	createOpts := new(common.ContainerCLIOpts)
 	query := struct {
 		Name string `schema:"name"`
 	}{
@@ -41,181 +38,131 @@ func CreateContainer(w http.ResponseWriter, r *http.Request) {
 	if len(input.HostConfig.Links) > 0 {
 		utils.Error(w, utils.ErrLinkNotSupport.Error(), http.StatusBadRequest, errors.Wrapf(utils.ErrLinkNotSupport, "bad parameter"))
 	}
-	newImage, err := runtime.ImageRuntime().NewFromLocal(input.Image)
+	_, err := runtime.ImageRuntime().NewFromLocal(input.Image)
 	if err != nil {
 		utils.Error(w, "Something went wrong.", http.StatusInternalServerError, errors.Wrap(err, "NewFromLocal()"))
 		return
 	}
-	containerConfig, err := runtime.GetConfig()
-	if err != nil {
-		utils.Error(w, "Something went wrong.", http.StatusInternalServerError, errors.Wrap(err, "GetConfig()"))
-		return
-	}
-	cc, err := makeCreateConfig(containerConfig, input, newImage)
-	if err != nil {
-		utils.Error(w, "Something went wrong.", http.StatusInternalServerError, errors.Wrap(err, "makeCreatConfig()"))
-		return
-	}
-	cc.Name = query.Name
-	utils.CreateContainer(r.Context(), w, runtime, &cc)
-}
 
-func makeCreateConfig(containerConfig *config.Config, input handlers.CreateContainerConfig, newImage *image2.Image) (createconfig.CreateConfig, error) {
-	var (
-		err  error
-		init bool
-	)
-	env := make(map[string]string)
-	stopSignal := unix.SIGTERM
-	if len(input.StopSignal) > 0 {
-		stopSignal, err = signal.ParseSignal(input.StopSignal)
-		if err != nil {
-			return createconfig.CreateConfig{}, err
-		}
-	}
+	// DEFAULTS
 
-	workDir := "/"
-	if len(input.WorkingDir) > 0 {
-		workDir = input.WorkingDir
-	}
+	// TODO We should probably set things from containers.conf here
 
-	stopTimeout := containerConfig.Engine.StopTimeout
+	createOpts.ReadOnlyTmpFS = true
+	createOpts.Systemd = "true"
+
+	// BASE CONFIG
+
+	createOpts.Hostname = input.Hostname
+	createOpts.User = input.User
+	createOpts.TTY = input.Tty
+	createOpts.Interactive = input.OpenStdin
+	createOpts.Env = input.Env
+	createOpts.Workdir = input.WorkingDir
+	createOpts.StopSignal = input.StopSignal
+
 	if input.StopTimeout != nil {
-		stopTimeout = uint(*input.StopTimeout)
-	}
-	c := createconfig.CgroupConfig{
-		Cgroups:      "", // podman
-		Cgroupns:     "", // podman
-		CgroupParent: "", // podman
-		CgroupMode:   "", // podman
-	}
-	security := createconfig.SecurityConfig{
-		CapAdd:             input.HostConfig.CapAdd,
-		CapDrop:            input.HostConfig.CapDrop,
-		LabelOpts:          nil,   // podman
-		NoNewPrivs:         false, // podman
-		ApparmorProfile:    "",    // podman
-		SeccompProfilePath: "",
-		SecurityOpts:       input.HostConfig.SecurityOpt,
-		Privileged:         input.HostConfig.Privileged,
-		ReadOnlyRootfs:     input.HostConfig.ReadonlyRootfs,
-		ReadOnlyTmpfs:      false, // podman-only
-		Sysctl:             input.HostConfig.Sysctls,
-	}
-
-	network := createconfig.NetworkConfig{
-		DNSOpt:       input.HostConfig.DNSOptions,
-		DNSSearch:    input.HostConfig.DNSSearch,
-		DNSServers:   input.HostConfig.DNS,
-		ExposedPorts: input.ExposedPorts,
-		HTTPProxy:    false, // podman
-		IP6Address:   "",
-		IPAddress:    "",
-		LinkLocalIP:  nil, // docker-only
-		MacAddress:   input.MacAddress,
-		// NetMode:      nil,
-		Network:      input.HostConfig.NetworkMode.NetworkName(),
-		NetworkAlias: nil, // docker-only now
-		PortBindings: input.HostConfig.PortBindings,
-		Publish:      nil, // podmanseccompPath
-		PublishAll:   input.HostConfig.PublishAllPorts,
-	}
-
-	uts := createconfig.UtsConfig{
-		UtsMode:  namespaces.UTSMode(input.HostConfig.UTSMode),
-		NoHosts:  false, //podman
-		HostAdd:  input.HostConfig.ExtraHosts,
-		Hostname: input.Hostname,
-	}
-
-	z := createconfig.UserConfig{
-		GroupAdd:   input.HostConfig.GroupAdd,
-		IDMappings: &storage.IDMappingOptions{}, // podman //TODO <--- fix this,
-		UsernsMode: namespaces.UsernsMode(input.HostConfig.UsernsMode),
-		User:       input.User,
-	}
-	pidConfig := createconfig.PidConfig{PidMode: namespaces.PidMode(input.HostConfig.PidMode)}
-	volumes := make([]string, 0, len(input.Volumes))
-	for k := range input.Volumes {
-		volumes = append(volumes, k)
-	}
-
-	// Docker is more flexible about its input where podman throws
-	// away incorrectly formatted variables so we cannot reuse the
-	// parsing of the env input
-	// [Foo Other=one Blank=]
-	for _, e := range input.Env {
-		splitEnv := strings.Split(e, "=")
-		switch len(splitEnv) {
-		case 0:
-			continue
-		case 1:
-			env[splitEnv[0]] = ""
-		default:
-			env[splitEnv[0]] = strings.Join(splitEnv[1:], "=")
+		if *input.StopTimeout < 0 {
+			utils.Error(w, "Something went wrong.", http.StatusBadRequest, errors.Errorf("must provide a stop timeout above 0, got %d", *input.StopTimeout))
 		}
+		createOpts.StopTimeout = uint(*input.StopTimeout)
 	}
 
-	// format the tmpfs mounts into a []string from map
-	tmpfs := make([]string, 0, len(input.HostConfig.Tmpfs))
-	for k, v := range input.HostConfig.Tmpfs {
-		tmpfs = append(tmpfs, fmt.Sprintf("%s:%s", k, v))
+	if input.AttachStdin {
+		createOpts.Attach = append(createOpts.Attach, "stdin")
+	}
+	if input.AttachStdout {
+		createOpts.Attach = append(createOpts.Attach, "stdout")
+	}
+	if input.AttachStderr {
+		createOpts.Attach = append(createOpts.Attach, "stderr")
 	}
 
-	if input.HostConfig.Init != nil && *input.HostConfig.Init {
-		init = true
+	// TODO: Is this right?
+	for vol := range input.Volumes {
+		createOpts.Volume = append(createOpts.Volume, vol)
 	}
 
-	m := createconfig.CreateConfig{
-		Annotations:   nil, // podman
-		Args:          nil,
-		Cgroup:        c,
-		CidFile:       "",
-		ConmonPidFile: "", // podman
-		Command:       input.Cmd,
-		UserCommand:   input.Cmd, // podman
-		Detach:        false,     //
-		// Devices:            input.HostConfig.Devices,
-		Entrypoint:        input.Entrypoint,
-		Env:               env,
-		HealthCheck:       nil, //
-		Init:              init,
-		InitPath:          "", // tbd
-		Image:             input.Image,
-		ImageID:           newImage.ID(),
-		BuiltinImgVolumes: nil, // podman
-		ImageVolumeType:   "",  // podman
-		Interactive:       false,
-		// IpcMode:           input.HostConfig.IpcMode,
-		Labels:    input.Labels,
-		LogDriver: input.HostConfig.LogConfig.Type, // is this correct
-		// LogDriverOpt:       input.HostConfig.LogConfig.Config,
-		Name:          input.Name,
-		Network:       network,
-		Pod:           "",    // podman
-		PodmanPath:    "",    // podman
-		Quiet:         false, // front-end only
-		Resources:     createconfig.CreateResourceConfig{},
-		RestartPolicy: input.HostConfig.RestartPolicy.Name,
-		Rm:            input.HostConfig.AutoRemove,
-		StopSignal:    stopSignal,
-		StopTimeout:   stopTimeout,
-		Systemd:       false, // podman
-		Tmpfs:         tmpfs,
-		User:          z,
-		Uts:           uts,
-		Tty:           input.Tty,
-		Mounts:        nil, // we populate
-		// MountsFlag:         input.HostConfig.Mounts,
-		NamedVolumes: nil, // we populate
-		Volumes:      volumes,
-		VolumesFrom:  input.HostConfig.VolumesFrom,
-		WorkDir:      workDir,
-		Rootfs:       "", // podman
-		Security:     security,
-		Syslog:       false, // podman
-
-		Pid: pidConfig,
+	if input.NetworkDisabled {
+		createOpts.Net.Network.NSMode = specgen.NoNetwork
 	}
-	return m, nil
+
+	for k, v := range input.Labels {
+		createOpts.Label = append(createOpts.Label, fmt.Sprintf("%s=%s", k, v))
+	}
+
+	// TODO: Domainname (what is this?)
+	// TODO: ExposedPorts (need to parse back into string)
+	// TODO: stdinOnce (I don't think we really support this)
+	// TODO: Cmd (pass straight into createconfig?)
+	// TODO Healthcheck (break up struct)
+	// TODO entrypoint (slice vs string)
+	// TODO Mac Address
+	// TODO onbuild (do we do anything with this?
+	// TODO Shell (build specific?)
+
+	// HOST CONFIG
+
+	createOpts.Volume = append(createOpts.Volume, input.HostConfig.Binds...)
+	createOpts.CIDFile = input.HostConfig.ContainerIDFile
+	createOpts.Rm = input.HostConfig.AutoRemove
+	createOpts.VolumesFrom = input.HostConfig.VolumesFrom
+	createOpts.CGroupsNS = string(input.HostConfig.CgroupnsMode)
+	createOpts.GroupAdd = input.HostConfig.GroupAdd
+	createOpts.IPC = string(input.HostConfig.IpcMode)
+	createOpts.OOMScoreAdj = input.HostConfig.OomScoreAdj
+	createOpts.PID = string(input.HostConfig.PidMode)
+	createOpts.Privileged = input.HostConfig.Privileged
+	createOpts.PublishAll = input.HostConfig.PublishAllPorts
+	createOpts.ReadOnly = input.HostConfig.ReadonlyRootfs
+	createOpts.SecurityOpt = input.HostConfig.SecurityOpt
+	createOpts.UTS = string(input.HostConfig.UTSMode)
+	createOpts.UserNS = string(input.HostConfig.UsernsMode)
+
+	if input.HostConfig.ShmSize != 0 {
+		createOpts.ShmSize = fmt.Sprintf("%d", input.HostConfig.ShmSize)
+	}
+
+	// TODO LogConfig
+	// TODO NetworkMode
+	// TODO PortBindings
+	// TODO RestartPolicy
+	// TODO VolumeDriver (We don't support this)
+	// TODO CapAdd/CapDrop
+	// TODO Capabilities (We don't support this yet)
+	// TODO DNS/DNSOptions/DNSSearch/ExtraHosts
+	// TODO Cgroup (is this cgroupparent?)
+	// TODO StorageOpt (We don't support this)
+	// TODO Tmpfs (different types)
+	// TODO sysctls
+	// TODO runtime (this is a global option for us)
+
+	// TODO Resources, Mounts, MaskedPaths, ReadonlyPaths, Init
+
+	// TODO Networking Config - we only support a lot of this for a single
+	// CNI network, so this should be simple
+
+	specgen := new(specgen.SpecGenerator)
+	specgen.Image = input.Image
+
+	if err := common.FillOutSpecGen(specgen, createOpts, input.Cmd); err != nil {
+		utils.Error(w, "Something went wrong.", http.StatusBadRequest, errors.Wrapf(err, "error parsing options"))
+	}
+
+	warnings, err := generate.CompleteSpec(r.Context(), runtime, specgen)
+	if err != nil {
+		utils.Error(w, "Something went wrong.", http.StatusBadRequest, errors.Wrapf(err, "error finalizing container configuration"))
+	}
+
+	ctr, err := generate.MakeContainer(r.Context(), runtime, specgen)
+	if err != nil {
+		utils.Error(w, "Something went wrong.", http.StatusBadRequest, errors.Wrapf(err, "error creating container"))
+	}
+
+	response := entities.ContainerCreateResponse{
+		ID:       ctr.ID(),
+		Warnings: warnings,
+	}
+
+	utils.WriteResponse(w, http.StatusCreated, response)
 }
